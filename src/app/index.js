31dbab93b86680c36cd83c842913edbf44bb967b
calebmbakwe <caleb.mbakwe@andela.com>
2019-03-01 19:15:31 +0100

    Added Redux to the ministry

---
+++ src/app/index.js
@@ -0,0 +1,6 @@
+import { store } from "./store";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Main } from "../components/Main";
+
+ReactDOM.render(<Main />, document.getElementById("app"));
